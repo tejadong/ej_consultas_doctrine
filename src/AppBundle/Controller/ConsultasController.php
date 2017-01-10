b2b266e91a225e6786191783806a327d43e60c88
Jesús Tejada Pérez <tejadong@gmail.com>
2017-01-10 14:39:50 +0100

    Añadir fecha de nacimiento en el listado.

--- src/AppBundle/Controller/ConsultasController.php
+++ src/AppBundle/Controller/ConsultasController.php
@@ -2,9 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class ConsultasController extends Controller
 {
@@ -15,6 +15,20 @@ class ConsultasController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $alumnado = $em->getRepository('AppBundle:Alumno')->findBy(['nombre' => 'María']);
+
+        /** @var EntityManager $em */
+        /*$em = $this->getDoctrine()->getManager();
+        $alumnado = $em->createQueryBuilder()
+            ->select('a')
+            ->from('AppBundle:Alumno', 'a')
+            ->where('a.nombre = :nombre')
+            ->setParameter('nombre', 'María')
+            ->getQuery()
+            ->getResult();*/
+
+        /*$alumnado = $em->createQuery('SELECT a FROM AppBundle:Alumno a WHERE a.nombre=:nombre ORDER BY a.apellidos DESC')
+            ->setParameter('nombre', 'María')
+            ->getResult();*/
         return $this->render('consultas/alumnado.html.twig', [
             'alumnado' => $alumnado
         ]);
